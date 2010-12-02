9d614b044e529373cbc02f4398da6228080f53c6
James Fargher <proglottis@nothing.co.nz>
2010-12-02 17:35:23 +1300

    Initial rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WineJournal::Application.config.secret_token = '994630d2b7d3329fe1708e5ffcdea253295cdba61e5ad7b5c3e98adeaa39ae2f89ebb8911910c0fb3673465f26db902c8f5c5b80beb08be751193bbdc89049aa'
