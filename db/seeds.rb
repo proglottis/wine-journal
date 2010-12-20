4ba0f55dc29e4ef6e357fc5dce8d0e9f1c1944ee
James Fargher <proglottis@nothing.co.nz>
2010-12-20 20:36:34 +1300

    Rank seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+User.create!(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password')
+
+Rank.create!(:name => 'Very Poor', :value => '1')
+Rank.create!(:name => 'Below average', :value => '2')
+Rank.create!(:name => 'Average', :value => '3')
+Rank.create!(:name => 'Above average', :value => '4')
+Rank.create!(:name => 'Above above average', :value => '5')
+Rank.create!(:name => 'Incredible', :value => '6')
