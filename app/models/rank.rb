161f7b64c6447b227db2a86dde0ba87897e9b67f
James Fargher <proglottis@nothing.co.nz>
2011-10-15 16:56:51 +1300

    Fix uniq query for postgres

--- app/models/rank.rb
+++ app/models/rank.rb
@@ -1,6 +1,6 @@
 class Rank < ActiveRecord::Base
   has_many :ratings
-  has_many :wines, :through => :ratings
+  has_many :wines, :through => :ratings, :uniq => true
 
   validates_presence_of :name, :value
 end
