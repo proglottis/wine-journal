5947488252722c69536b569d215a04bda3b5a2cd
James Fargher <proglottis@nothing.co.nz>
2011-10-15 17:37:21 +1300

    Use asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,9 @@ module WineJournal
 
     # Activate observers that should always be running.
     # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
+    
+    config.assets.enabled = true
+    config.assets.version = '1.0'
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -32,4 +32,8 @@ WineJournal::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  config.serve_static_assets = true
+  config.static_cache_control = "public, max-age=3600"
+  config.assets.allow_debugging = true
 end
