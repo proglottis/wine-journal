534ee5241de28f6725223768a3d04b2cf38dcb59
James Fargher <proglottis@gmail.com>
2015-08-12 20:50:12 +1200

    Fix sitemap generation

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,33 +1,8 @@
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "http://redwine.nothing.co.nz"
 
-SitemapGenerator::Sitemap.add_links do |sitemap|
-  # Put links creation logic here.
-  #
-  # The root path '/' and sitemap index file are added automatically.
-  # Links are added to the Sitemap in the order they are specified.
-  #
-  # Usage: sitemap.add(path, options={})
-  #        (default options are used if you don't specify)
-  #
-  # Defaults: :priority => 0.5, :changefreq => 'weekly',
-  #           :lastmod => Time.now, :host => default_host
-  # 
-  # 
-  # Examples:
-  # 
-  # Add '/articles'
-  #   
-  #   sitemap.add articles_path, :priority => 0.7, :changefreq => 'daily'
-  #
-  # Add individual articles:
-  #
-  #   Article.find_each do |article|
-  #     sitemap.add article_path(article), :lastmod => article.updated_at
-  #   end
-
+SitemapGenerator::Sitemap.create do |sitemap|
   Wine.find_each do |w|
     sitemap.add wine_path(w), :lastmod => w.updated_at
   end
-
 end
